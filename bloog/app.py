bc9eb215961f0092716ff1d663031c931efeefe3
Daniel Malik <daniel.malik.malik@gmail.com>
2017-01-31 14:38:35 +0100

    user table i db, registration new user (name not unique yet) and login if user exists. 2 user classes : admin=1 or admin = 0), next add restrictions to creating new posts - only admin=1, and to read - olny logged

--- bloog/app.py
+++ bloog/app.py
@@ -1,11 +1,25 @@
 from flask import Flask, request, g, redirect, url_for, abort, flash, render_template, make_response, session, escape
 import os
 import sqlite3
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
 
-
+secret_value = '3i1415'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+# in future I will move forms to separate module
+
+class RegistrationForm(Form):
+    username = StringField('Name', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=35)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    secret = StringField('Secret', [validators.Length(max=25), validators.Optional()])
+    accept_tos = BooleanField('I accept everything', [validators.DataRequired()])
 
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'nolablog.db'),
@@ -14,6 +28,7 @@ app.config.update(dict(
     PASSWORD='coderslab'
 ))
 app.config.from_envvar('APP_SETTINGS', silent=True)
+
 
 def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
@@ -42,7 +57,24 @@ def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
-@app.route("/")
+def allusers():
+    db = get_db()
+    all_users = []
+    cursor = db.execute('select * from users')
+    all_users = cursor.fetchall()
+    for row in cursor:
+        all_users.append(row[1])
+    return all_users
+
+def isuser(user):
+    isuser = (user,)
+    db = get_db()
+    all_users = []
+    cursor = db.execute('select * from users where name=?', isuser)
+    all_users = cursor.fetchall()
+    return all_users
+
+
 @app.route("/")
 def main():
     if 'username' in session:
@@ -75,14 +107,18 @@ def add_entry():
 def login():
     error = None
     user = None
+    all_users = allusers()
+
     if request.method == 'GET':
 
         return render_template('login.html')
     elif request.method == 'POST':
-        if request.form['username'] != 'admin':
+        user = request.form['username']
+        print(isuser(user))
+        print(isuser(user) == [])
+        if isuser(user) == []:
             error = 'Wrong user'
-        elif request.form['password'] != 'admin':
-            error = 'Wrong password'
+            return render_template('login.html')
         else:
             session['username'] = request.form['username']
             session['logged_in'] = True
@@ -97,6 +133,43 @@ def logout():
     session.pop('username', None)
     flash('You were logged out')
     return redirect(url_for('show_entries'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    error = None
+    form = RegistrationForm(request.form)
+
+    if request.method =='POST' and form.validate():
+        user = request.form['username']
+        mail = request.form['email']
+        password = request.form['password']
+        secret = request.form['secret']
+        db = get_db()
+        cur = db.execute('select name from users')
+        all_users = cur.fetchall()
+        if user not in all_users:
+            if secret == secret_value:
+                admin = 1
+
+                db = get_db()
+                db.execute('insert into users (name, mail, password, admin) values (?, ?, ?, ?)',
+                           [user, mail, password, admin])
+                db.commit()
+                return redirect(url_for('login'))
+            else:
+                no_admin = 0
+
+                db = get_db()
+                db.execute('insert into users (name, mail, password, admin) values (?, ?, ?, ?)',
+                           [user, mail, password, no_admin])
+                db.commit()
+                return redirect(url_for('login'))
+        else:
+            error = "wrong user"
+            return render_template('register.html', form=form, errors=error)
+    else:
+        return render_template('register.html', form=form, errors=error)
+
 
 if __name__ == "__main__":
     app.secret_key = 'super secret key'
--- bloog/schema.sql
+++ bloog/schema.sql
@@ -5,3 +5,12 @@ create table entries (
   'text' text not null,
   'link' text
 );
+
+drop table if exists users;
+create table users (
+  id integer primary key autoincrement,
+  name text not null,
+  'mail' text not null,
+  'password' text not null,
+  'admin' integer not null
+);
